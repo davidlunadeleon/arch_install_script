d956859429d02171c321e0e5b467cb2ceb2c453b
David Luna de León <davidlunadeleon@gmail.com>
2022-07-06 11:44:33 -0500

    Update mounting script
    - Match new lvm structure
    - Update efi partition mounting point
    - Mount home

--- scripts/mounting.sh
+++ scripts/mounting.sh
@@ -4,57 +4,31 @@
 # Mount all partitions and handle the creation of btrfs subvolumes
 ###############################################################################
 
-echo Mounting partitions...
+echo "
+############################################################
+#
+# Mounting partitions and starting swap...
+#
+############################################################
+"
 
-echo Creating subvolumes...
+mount /dev/mapper/main-root /mnt
 
-mount LABEL=root /mnt
+mkdir /mnt/home
+mount /dev/mapper/main-home /mnt/home
 
-# Subvolumes used for backups
-btrfs subvolume create /mnt/@
-btrfs subvolume create /mnt/@home
-btrfs subvolume create /mnt/@snapshots
+mkdir /mnt/boot
+mount /dev/mapper/crypto-boot /mnt/boot
 
-# Subvolumes excluded from backups
-mkdir -p /mnt/var
-btrfs subvolume create /mnt/var/cache
-btrfs subvolume create /mnt/var/log
-btrfs subvolume create /mnt/var/tmp
-
-umount /mnt
-
-echo Done creating subvolumes
-
-# Mount previously created subvolumes
-mount_subvolume() {
-	echo Mounting $2
-	mkdir -p $1
-	mount -o compress=zstd,subvol=$2 /dev/mapper/root $1
-}
-
-echo Mounting subvolumes...
-
-echo Mounting @
-mount -o compress=zstd,subvol=@ /dev/mapper/root /mnt
-mount_subvolume /mnt/home @home
-mount_subvolume /mnt/.snapshots @snapshots
-mount_subvolume /mnt/var/cache var/cache
-mount_subvolume /mnt/var/log var/log
-mount_subvolume /mnt/var/tmp var/tmp
-
-echo Done mounting subvolumes
-
-echo Mounting efi partition...
-
-mkdir -p /mnt/efi
+mkdir /mnt/efi
 mount ${AIS_MAIN_DRIVE}1 /mnt/efi
 
-echo Done mounting efi partition
+swapon /dev/mapper/main-swap
 
-echo Enabling swap...
-
-swapon /dev/mapper/swap
-
-echo Done enabling swap
-
-echo Done mounting partitions
+echo "
+############################################################
+#
+# Done mounting partitions
+#
+############################################################
+"
