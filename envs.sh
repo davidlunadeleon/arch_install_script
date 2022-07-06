23b2d51af82658db8226cfe0d49bbf42a6dfc814
David Luna de León <davidlunadeleon@gmail.com>
2022-07-06 11:07:41 -0500

    Update envs variables

--- envs.sh
+++ envs.sh
@@ -11,10 +11,11 @@ export AIS_OTHER_DRIVES=""
 
 # Swap configuration
 export AIS_SWAP_SIZE=4G
+export AIS_ROOT_SIZE=16G
 
 # LUKS Encryption passphrases
-export AIS_ROOT_KEY=default
-export AIS_SWAP_KEY=default
+export AIS_BOOT_KEY=default
+export AIS_SYSTEM0_KEY=default
 # Additional passprhases, if any. Must match the size of OTHER_DRIVES. String
 # with semicolon separated elements.
 export AIS_OTHER_DRIVES_KEYS=""
@@ -31,7 +32,7 @@ export AIS_LOCALE_GEN_LOCALES="en_US.UTF-8 UTF-8;es_MX.UTF-8 UTF-8"
 export AIS_LOCALE=en_US.UTF-8
 export AIS_TIMEZONE=America/Monterrey
 export AIS_USERNAME=user
-export AIS_ROOT_PASSWORD=default
-export AIS_USER_PASSWORD=default
 # Either amd or intel, or leave empty if none should be installed
 export AIS_MICROCODE=
+# Either intel, nvidia, nouveau, or amd
+export AIS_GRAPHICS_DRIVERS=
