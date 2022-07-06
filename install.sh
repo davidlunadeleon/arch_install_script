27b39550870818c03da249a2473d8abf872deb06
David Luna de León <davidlunadeleon@gmail.com>
2022-07-06 09:02:33 -0500

    Update install script
    - Improve messages.
    - Remove repeated lines.

--- install.sh
+++ install.sh
@@ -4,19 +4,17 @@
 mkdir logs/
 touch logs/arch_install_script.log
 
-message="ArchLinux install script..."
-echo $message
-echo $message >> logs/arch_install_script.log
-
+message="
+############################################################
+#
+#	ArchLinux install script...
+#
+############################################################
+"
+echo $message > >(tee -a logs/arch_install_script.log)
 
 chmod +x envs.sh
-chmod +x scripts/partitions.sh
-chmod +x scripts/encryption.sh
-chmod +x scripts/formatting.sh
-chmod +x scripts/mounting.sh
-chmod +x scripts/pacstrap.sh
-chmod +x scripts/arch-chroot.sh
-chmod +x scripts/cleanup.sh
+chmod -R +x scripts/
 
 # Load variables
 source envs.sh
@@ -36,11 +34,17 @@ genfstab -U /mnt >> /mnt/etc/fstab
 mkdir -p /mnt/root/scripts/
 cp envs.sh /mnt/root/scripts/envs.sh
 cp scripts/arch-chroot.sh /mnt/root/scripts/arch-chroot.sh
+ln -sf /run/systemd/resolve/stub-resolve.conf /mnt/etc/resolv.conf
 
 
 arch-chroot /mnt /root/scripts/arch-chroot.sh > >(tee -a logs/arch_install_script.log)
 source scripts/cleanup.sh > >(tee -a logs/arch_install_script.log)
 
-message="Done installing ArchLinux. Goodbye!"
-echo $message
-echo $message >> logs/arch_install_script.log
+message="
+############################################################
+#
+#	Done installing ArchLinux. Goodbye!
+#
+############################################################
+"
+echo $message > >(tee -a logs/arch_install_script.log)
