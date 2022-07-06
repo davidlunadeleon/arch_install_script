dac0fe6309c84d40688cf09c7eb53774d3864832
David Luna de León <davidlunadeleon@gmail.com>
2022-07-06 11:44:06 -0500

    Update packages installed in pacstrap step

--- scripts/pacstrap.sh
+++ scripts/pacstrap.sh
@@ -4,22 +4,33 @@
 # Install all necessary packages previous to doing arch-chroot
 ###############################################################################
 
+echo "
+############################################################
+#
+# Installing essential packages using pacstrap. Installing
+# the following packages and package groups:
+#	- base
+#	- linux
+#	- linux-firmware
+#	- lvm2
+#	- grub
+#	- efibootmgr
+#	- sudo
+#	- base-devel
+#
+############################################################
+"
+
 # Essential packages per the installatiion guide
 pacstrap /mnt base linux linux-firmware
 
 # Other important packages
-pacstrap /mnt grub btrfs-progs efibootmgr sudo base-devel git
+pacstrap /mnt lvm2 grub efibootmgr sudo base-devel
 
-# Sound
-pacstrap /mnt pipewire pipewire-alsa pipewire-pulse
-
-# Display
-pacstrap /mnt wayland sway bemenu mako swaybg swayidle waybar swaylock \
-	xorg-xwayland i3status
-
-# User preference
-pacstrap /mnt zsh vim firefox kitty rust python typescript go gcc neovim
-
-if [ -n "$AIS_NETWORK_SSID" ]; then
-	pacstrap /mnt iwd
-fi
+echo "
+############################################################
+#
+# Done installing essential packages
+#
+############################################################
+"
