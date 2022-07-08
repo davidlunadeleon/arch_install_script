81ffefc86b3ee0fb2116ff265edca106c56a36d9
David Luna de León <davidlunadeleon@gmail.com>
2022-07-07 19:57:56 -0500

    Update cleanup script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -7,14 +7,17 @@
 echo Unmounting partitions...
 
 umount -R /mnt
-swapoff /dev/mapper/swap
+swapoff /dev/mapper/main-swap
 
 echo Done unmounting partitions
 
 echo Closing encrypted partitions...
 
-cryptsetup close swap
-cryptsetup close root
+cryptsetup close main-home
+cryptsetup close main-root
+cryptsetup close main-swap
+cryptsetup close crypto-boot
+cryptsetup close crypto-system0
 IFS=';' read -ra ais_mapped_names <<< "$AIS_MAPPED_NAMES"
 for name in "${ais_mapped_names[@]}"; do
 	cryptsetup close $name
